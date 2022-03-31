ca5c25b0033b3e2650acfa9abf1a4503c7bfb07d
Givrali2 <99682111+Givrali2@users.noreply.github.com>
2022-03-31 19:14:29 +0200

    Add files via upload

---
+++ ProjectChess/src/gui/gui/ChessTime.java
@@ -0,0 +1,57 @@
+package gui;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class ChessTime {
+    public int minute;
+    public int seconde;
+    public Timer Mytimer;
+    public WindowTheEnd FinDeLaPartie;
+    private TimerTask task;
+
+    public ChessTime(int m, int s) {
+        minute = m;
+        seconde = s;
+        Mytimer = new Timer();
+
+
+
+        this.task = new TimerTask() {
+            int counter = seconde;
+
+            public void run() {
+                if (counter > 0) {
+                    counter--;
+                    seconde = counter;
+                    System.out.println("" + minute + " " + seconde);
+                } else {
+                    if (counter == 0 && minute != 0) {
+                        minute--;
+                        counter = 59;
+                        seconde = 59;
+                        System.out.println("" +minute +" " + seconde);
+                    } else {
+                        FinDeLaPartie = new WindowTheEnd();
+                        FinDeLaPartie.setVisible(true);
+                        Mytimer.cancel();
+                    }
+                }
+            }
+        };
+
+    }
+
+    public void start() {
+        Mytimer.scheduleAtFixedRate(this.task, 0, 1000);
+    }
+    
+    public void freezetime() {
+        Mytimer.stop();
+    }
+
+    public static void main(String[] args) {
+        ChessTime time = new ChessTime(0, 10);
+        time.start();
+    }
+}
