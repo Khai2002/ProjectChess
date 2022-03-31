539d0481068e366b628dabed1ab9a136483305f6
LHThunder <99684874+LHThunder@users.noreply.github.com>
2022-03-31 08:27:22 +0200

    Update Move.java

--- ProjectChess/src/move/Move.java
+++ ProjectChess/src/move/Move.java
@@ -25,6 +25,7 @@ public class Move implements Serializable {
         this.startingTile = startingTile;
         this.destinationTile = destinationTile;
         this.type = 0;
+
     }
 
     public Move(){
@@ -82,56 +83,8 @@ public class Move implements Serializable {
 //    }
 
     public String toString(){
-        if(this.type == 0){
-            String notation = "";
-            notation += Board.PIECE_NOTATION[Math.abs(this.piece.id) - 1];
-            /*
-            if (board.colorActive == 1 && board != null) {
-                for (Move move : board.whiteMoves) {
-                    if (move.piece.id == this.piece.id && move.destinationTile.equals(this.destinationTile)) {
-                        if (move.startingTile.tileCoordinate[0] == this.startingTile.tileCoordinate[0]) {
-                            notation += Board.ROW_NOTATION[7 - this.startingTile.tileCoordinate[1]];
-                        }
-                        if (move.startingTile.tileCoordinate[1] == this.startingTile.tileCoordinate[1]) {
-                            notation += Board.COLUMN_NOTATION[this.startingTile.tileCoordinate[0]];
-                        }
-                    }
-                }
-            } else {
-                for (Move move : board.blackMoves) {
-                    if (move.piece.id == this.piece.id && move.destinationTile.equals(this.destinationTile)) {
-                        if (move.startingTile.tileCoordinate[0] == this.startingTile.tileCoordinate[0]) {
-                            notation += Board.ROW_NOTATION[8 - this.startingTile.tileCoordinate[1]];
-                        }
-                        if (move.startingTile.tileCoordinate[1] == this.startingTile.tileCoordinate[1]) {
-                            notation += Board.COLUMN_NOTATION[this.startingTile.tileCoordinate[0]];
-                        }
-                    }
-                }
-            }
-            */
-            if(this.board != null) {
-                if (this.board.board[this.destinationTile.tileCoordinate[0]][this.destinationTile.tileCoordinate[1]] instanceof Tile.OccupiedTile) {
-                    notation += 'x';
-                }
-            }
-            notation += Board.COLUMN_NOTATION[this.destinationTile.tileCoordinate[1]];
-            notation += Board.ROW_NOTATION[7 - this.destinationTile.tileCoordinate[0]];
-            return notation;
-        }else if (this.type == 2){
-            return "";
-        }else if(this.type == 1){
-            if(affectedPiece.position[1]>piece.position[1]){
-                return "0-0";
-            }else{
-                return "0-0-0";
-            }
-        }else if(this.type == 3){
-            return "Promotion";
-        }else{
-            return this.piece.name + this.startingTile.tileCoordinate[0] + this.startingTile.tileCoordinate[1]+ "-"+ this.destinationTile.tileCoordinate[0] + this.destinationTile.tileCoordinate[1];
-        }
 
+        return board.notation;
 
     }
 
